947465d99cb4153943b9b89f5a020093b56a874e
Christof93 <christofbless@gmail.com>
2024-04-12 00:13:25 +0200

    import json file

--- demonstrator/src/components/GraphViz.vue
+++ demonstrator/src/components/GraphViz.vue
@@ -1,46 +1,38 @@
 <script setup>
 import  { useNodeStore, isCorrectCategory } from '../stores/nodeStore'
+import data from './acl2022.json'
+
 const nodeStore = useNodeStore()
 const myGraph = ForceGraph3D({controlType: "trackball"});
-fetch('acl2022.json')
-  .then(response => {
-      if (!response.ok) {
-          throw new Error('Network response was not ok');
-      }
-      return response.json();
+
+data = data[0].graph_data
+const nodeResolution = Math.floor(100/Math.log(data.nodes.length+2))
+console.log(data);
+initBalances(data.nodes)
+myGraph(document.getElementById('graphViz'))
+  .graphData(data)
+  .nodeResolution(nodeResolution)
+  .nodeColor((n) => {return nodeStore.nodeColorOn[n._type[0]]})
+  .nodeVal((n) => {
+    return nodeStore.getNodeSizeMap(data.nodes.length)[n._type[0]]
   })
-  .then(data => {
-    data = data[0].graph_data
-    const nodeResolution = Math.floor(100/Math.log(data.nodes.length+2))
-    console.log(data);
-    initBalances(data.nodes)
-    myGraph(document.getElementById('graphViz'))
-      .graphData(data)
-      .nodeResolution(nodeResolution)
-      .nodeColor((n) => {return nodeStore.nodeColorOn[n._type[0]]})
-      .nodeVal((n) => {
-        return nodeStore.getNodeSizeMap(data.nodes.length)[n._type[0]]
-      })
-      .linkLabel('_type')
-    //   .numDimensions(2)
-      .enableNodeDrag(false)
-      //   .forceEngine('ngraph')
-      .onNodeClick((node)=>{
-        nodeStore.nodeInfo = makeInfoNode(node)
-        if (nodeStore.lookingForSpender && isCorrectCategory(nodeStore, node)) {
-          nodeStore.currentAccount = node
-          nodeStore.stopLookingForSpender()
-        }
-        else if (nodeStore.lookingForRecipient && isCorrectCategory(nodeStore, node)) {
-          nodeStore.currentRecipient = node
-          nodeStore.stopLookingForRecipient()
-        }
-      })
-    nodeStore.graphViz=myGraph
+  .linkLabel('_type')
+//   .numDimensions(2)
+  .enableNodeDrag(false)
+  //   .forceEngine('ngraph')
+  .onNodeClick((node)=>{
+    nodeStore.nodeInfo = makeInfoNode(node)
+    if (nodeStore.lookingForSpender && isCorrectCategory(nodeStore, node)) {
+      nodeStore.currentAccount = node
+      nodeStore.stopLookingForSpender()
+    }
+    else if (nodeStore.lookingForRecipient && isCorrectCategory(nodeStore, node)) {
+      nodeStore.currentRecipient = node
+      nodeStore.stopLookingForRecipient()
+    }
   })
-  .catch(error => {
-    console.error('There was a problem with your fetch operation:', error);
-  });
+nodeStore.graphViz=myGraph
+
 
 
 function makeInfoNode(node) {
